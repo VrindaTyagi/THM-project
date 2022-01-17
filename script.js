a247d030ffe61f44cdff8d960f8f2618c1365804
Amrita <amrita@code.com>
2022-01-17 17:47:53 +0530

    latest

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
-const recipeCloseBtn = document.getElementById('recipe-close-btn');
-
+// const recipeCloseBtn = document.getElementById('recipe-close-btn');
 // event listeners ,add functionality to buttons
 searchBtn.addEventListener('click', getMealList);
-mealList.addEventListener('click', getMealRecipe);
+// mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
-
+var cart = document.getElementById('basket');
+var select = document.querySelector('.select');
+var add = document.querySelector('.recipe-btn');
+for(var but of add)
+    {
+        but.addEventListener('click', (e)=>{
+            var item = Number(cart.getAttribute('data-count') || 0);
+            cart.setAttribute('data-count', item + 1);
+            cart.classList.add('on')
+        })
+    }
 // get meal list that matches with the ingredients
 function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
@@ -32,12 +41,15 @@ function getMealList(){
                             <h3>${meal.desc}</h3>
                             </br>
                             <h4>${meal.price}</h4>
-                            <a href = "#" class = "recipe-btn">Add to Cart</a>
+                            <p>
+                            <button  class="recipe-btn">Add to Cart</button>
+                            </p>
                         </div>
                     </div>
+                    
                 `;
             });
-          
+         
             mealList.classList.remove('notFound');
         } else{
             html = "Sorry, we didn't find any meal!";
@@ -46,38 +58,4 @@ function getMealList(){
 
         mealList.innerHTML = html;
     });
-}
-
-
-// get recipe of the meal
-// function getMealRecipe(e){
-//     e.preventDefault();
-//     if(e.target.classList.contains('recipe-btn')){
-//         let mealItem = e.target.parentElement.parentElement;
-//         fetch("app1.json?i=${mealItem.dataset.id}")
-//         .then(response => response.json())
-//         .then(data => mealRecipeModal(data.meals));
-//     }
-// }
-
-// create a modal
-function mealRecipeModal(meal){
-    console.log(meal);
-    meal = meal[0];
-    let html = `
-        <h2 class = "recipe-title">${meal.title}</h2>
-        <p class = "recipe-category">${meal.category}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.desc}</p>
-        </div>
-        <div class = "recipe-meal-img">
-            <img src = "${meal.img}" alt = "">
-        </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
-    mealDetailsContent.innerHTML = html;
-    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
